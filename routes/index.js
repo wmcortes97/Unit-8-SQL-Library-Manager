b4852a6df4224f2a59fcca399ecd9ebcd42668c9
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-11-03 11:32:02 -0700

    all routes working
    basic functionality is working

--- routes/index.js
+++ routes/index.js
@@ -57,34 +57,51 @@ router.get(
   })
 );
 
-// /*POST updates book info in the database */
-// router.post(
-//   "/books/:id",
-//   asyncHandler(async (req, res) => {
-//     let book;
-//     try {
-//       book = await Book.findByPk(req.params.id);
-//       if (book) {
-//         await book.update(req.body);
-//         res.redirect("/books/" + book.id);
-//       } else {
-//         res.sendStatus(404);
-//       }
-//     } catch (error) {
-//       if (error.name === "SequelizeValidationError") {
-//         book = await Book.build(req.body);
-//         book.id = req.params.id;
-//         res.render("form-error", { book, error: error.errors });
-//       } else {
-//         throw error;
-//       }
-//     }
-//   })
-// );
+/*POST updates book info in the database */
+router.post(
+  "/books/:id",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
 
-// /*POST deletes a book */
-// router.get("/books/:id/delete", function (req, res, next) {
-//   asyncHandler(async (req, res) => {});
-// });
+    if (book) {
+      await book.update(req.body);
+      res.redirect("/");
+    } else {
+      res.sendStatus(404);
+    }
+    // let book;
+    // try {
+    //   book = await Book.findByPk(req.params.id);
+    //   if (book) {
+    //     await book.update(req.body);
+    //     res.redirect("/books/" + book.id);
+    //   } else {
+    //     res.sendStatus(404);
+    //   }
+    // } catch (error) {
+    //   if (error.name === "SequelizeValidationError") {
+    //     book = await Book.build(req.body);
+    //     book.id = req.params.id;
+    //     res.render("form-error", { book, error: error.errors });
+    //   } else {
+    //     throw error;
+    //   }
+    // }
+  })
+);
+
+/*POST deletes a book */
+router.post(
+  "/books/:id/delete",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.destroy();
+      res.redirect("/");
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
 
 module.exports = router;
